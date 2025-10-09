735f685786164aee2957e868ecc0fe8a99123a98
AdamChen <powq2011@gmail.com>
2025-10-09 14:49:27 +0800

    [SnakeGame]: refactor Special Food convert to text

--- Snake/app/src/main/java/com/adam/app/snake/model/SpecialFood.java
+++ Snake/app/src/main/java/com/adam/app/snake/model/SpecialFood.java
@@ -1,8 +1,8 @@
 /**
  * Copyright 2015 the Adam Game
- *
+ * <p>
  * Description: This class is the Special food model
- *
+ * <p>
  * Author: Adam Chen
  * Date: 2025/10/01
  */
@@ -11,35 +11,12 @@ package com.adam.app.snake.model;
 import java.util.Objects;
 
 public final class SpecialFood {
-    /**
-     * class TYPE
-     * SPEED_UP
-     * SLOW_DOWN
-     * SHORTEN
-     * EXTEND
-     * INVINCIBLE
-     * INVISIBLE
-     * SCORE_DOUBLE
-     * BOMB
-     */
-    public static class TYPE {
-        public static final int SPEED_UP = 0;
-        public static final int SLOW_DOWN = 1;
-        public static final int SHORTEN = 2;
-        public static final int EXTEND = 3;
-        public static final int INVINCIBLE = 4;
-        public static final int INVISIBLE = 5;
-        public static final int SCORE_DOUBLE = 6;
-        public static final int BOMB = 7;
-    }
-
     // x int
     private final int mX;
     // y int
     private final int mY;
     // type TYPE
     private final int mType;
-
     /**
      * constructor
      *
@@ -67,9 +44,8 @@ public final class SpecialFood {
     }
 
     public String toText() {
-        return Objects.requireNonNull(Convert.fromType(mType)).toText();
+        return Objects.requireNonNull(Convert.fromType(mType)).name();
     }
-
 
     /**
      *
@@ -86,62 +62,20 @@ public final class SpecialFood {
 
 
     private enum Convert {
-        SpeedUp(TYPE.SPEED_UP) {
-            @Override
-            String toText() {
-                return "SpeedUp";
-            }
-        },
-        SlowDown(TYPE.SLOW_DOWN) {
-            @Override
-            String toText() {
-                return "SlowDown";
-            }
-        },
-        Shorten(TYPE.SHORTEN) {
-            @Override
-            String toText() {
-                return "Shorten";
-            }
-        },
-        Extend(TYPE.EXTEND) {
-            @Override
-            String toText() {
-                return "Extend";
-            }
-        },
-        Invincible(TYPE.INVINCIBLE) {
-            @Override
-            String toText() {
-                return "Invincible";
-            }
-        },
-        Invisible(TYPE.INVISIBLE) {
-            @Override
-            String toText() {
-                return "Invisible";
-            }
-        },
-        ScoreDouble(TYPE.SCORE_DOUBLE) {
-            @Override
-            String toText() {
-                return "ScoreDouble";
-            }
-        },
-        Bomb(TYPE.BOMB) {
-            @Override
-            String toText() {
-                return "Bomb";
-            }
-        };
+        SpeedUp(TYPE.SPEED_UP),
+        SlowDown(TYPE.SLOW_DOWN),
+        Shorten(TYPE.SHORTEN),
+        Extend(TYPE.EXTEND),
+        Invincible(TYPE.INVINCIBLE),
+        Invisible(TYPE.INVISIBLE),
+        ScoreDouble(TYPE.SCORE_DOUBLE),
+        Bomb(TYPE.BOMB);
 
         private int mType;
 
         private Convert(int type) {
             mType = type;
         }
-
-        abstract String toText();
 
         static Convert fromType(int type) {
             for (Convert convert : Convert.values()) {
@@ -152,6 +86,28 @@ public final class SpecialFood {
             return null;
         }
 
+    }
+
+    /**
+     * class TYPE
+     * SPEED_UP
+     * SLOW_DOWN
+     * SHORTEN
+     * EXTEND
+     * INVINCIBLE
+     * INVISIBLE
+     * SCORE_DOUBLE
+     * BOMB
+     */
+    public static class TYPE {
+        public static final int SPEED_UP = 0;
+        public static final int SLOW_DOWN = 1;
+        public static final int SHORTEN = 2;
+        public static final int EXTEND = 3;
+        public static final int INVINCIBLE = 4;
+        public static final int INVISIBLE = 5;
+        public static final int SCORE_DOUBLE = 6;
+        public static final int BOMB = 7;
     }
 
 
