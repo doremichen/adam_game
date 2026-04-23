ca995bb1508464590ec7d63ab823135b338e69ca
AdamChen <powq2011@gmail.com>
2026-04-23 13:13:45 +0800

    [Galaga]: Fix press navigation button issue: No action when the multiple press

--- Galaga/app/src/main/java/com/adam/app/galaga/ui/adapter/UIBinding.java
+++ Galaga/app/src/main/java/com/adam/app/galaga/ui/adapter/UIBinding.java
@@ -22,6 +22,7 @@
 
 package com.adam.app.galaga.ui.adapter;
 
+import android.graphics.Rect;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.OvershootInterpolator;
@@ -38,6 +39,8 @@ import java.util.Locale;
 
 public class UIBinding {
 
+    private static int mActiveDirectionId = View.NO_ID;
+
     @BindingAdapter("onTouchDirection")
     public static void setOnTouchDirection(View view, GameViewModel viewModel) {
         // resource id
@@ -50,19 +53,42 @@ public class UIBinding {
             public boolean onTouch(View v, MotionEvent event) {
                 switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
+                        mActiveDirectionId = id;
                         vm.setMoveDirection(direction);
                         v.setPressed(true);
                         break;
+                    case MotionEvent.ACTION_MOVE:
+                        Rect rect = new Rect(v.getLeft(), v.getTop(), v.getRight(), v.getBottom());
+                        // point position
+                        int x = v.getLeft() + (int) event.getX();
+                        int y = v.getTop() + (int) event.getY();
+                        // boundary check
+                        if (!rect.contains(x, y)) {
+                            handleRelease();
+                        }
+                        break;
                     case MotionEvent.ACTION_UP:
                     case MotionEvent.ACTION_CANCEL:
-                        vm.setMoveDirection(null);
-                        v.setPressed(false);
+                        handleRelease();
                         break;
                     default:
                         break;
                 }
                 return true;
             }
+
+
+            private void handleRelease() {
+                // view
+                view.setPressed(false);
+
+                if (mActiveDirectionId == id) {
+                    viewModel.setMoveDirection(null);
+                    mActiveDirectionId = View.NO_ID;
+                }
+            }
+
+
         });
     }
 
